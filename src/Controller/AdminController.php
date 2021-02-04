88166e7856338c5ceb2da9f42a211e830a85f6af
Aline Denizot <aline.denizot2@gmail.com>
2021-02-04 15:22:59 +0100

    dashboard admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,11 +8,14 @@ use App\Entity\Article;
 use App\Entity\Comment;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use App\Service\Slugify;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 
 /**
@@ -37,7 +40,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/newArticle", name="article_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, Slugify $slugify): Response
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
@@ -46,88 +49,19 @@ class AdminController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
 
-            $slug = $slugify->slug($category->getTitle());
+            $slug = $slugify->slug($article->getTitle());
             $article->setSlug($slug);
 
             $entityManager->persist($article);
             $entityManager->flush();
 
-            return $this->redirectToRoute('dashboard');
+            return $this->redirectToRoute('dashboard_index');
         }
 
         return $this->render('article/new.html.twig', [
             'article' => $article,
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("article/{slug}/edit", name="article_edit", methods={"GET","POST"})
-     */
-    public function editArticle(Request $request, Article $article): Response
-    {
-        $form = $this->createForm(ArticleType::class, $article);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('dashboard');
-        }$slug = $slugify->slug($category->getName());
-            $category->setSlug($slug);
-
-        return $this->render('article/edit.html.twig', [
-            'article' => $article,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("article/{slug}", name="article_delete", methods={"DELETE"})
-     */
-    public function deleteArticle(Request $request, Article $article): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($article);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('dashboard');
-    }
-
-    /**
-     * @Route("comment/{id}/edit", name="comment_edit", methods={"GET","POST"})
-     */
-    public function editComment(Request $request, Comment $comment): Response
-    {
-        $form = $this->createForm(Comment1Type::class, $comment);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('dashboard');
-        }
-
-        return $this->render('comment/edit.html.twig', [
-            'comment' => $comment,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("comment/{id}", name="comment_delete", methods={"DELETE"})
-     */
-    public function deleteComment(Request $request, Comment $comment): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($comment);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('dashboard');
     }
 
 }
